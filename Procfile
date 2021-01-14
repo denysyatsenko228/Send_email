8946228a997edfc0fb8e17d488dd3f5e43832989
Denys Yatsenko <dayyz@bk.ru>
2021-01-14 18:48:02 +0200

    release with text_resume

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn send_email.wsgi --log-file -
--- send_email/settings.py
+++ send_email/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
